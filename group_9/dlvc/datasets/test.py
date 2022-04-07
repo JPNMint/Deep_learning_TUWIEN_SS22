8a1a2c15a61942aaaf98c41571e76c19930c6622
Michael Koepf <michael.koepf@student.tuwien.ac.at>
2022-04-07 16:04:57 +0200

    Removed test.py
    [Issue #1]

--- group_9/dlvc/datasets/test.py
+++
@@ -1,8 +0,0 @@
-def unpickle(file):
-    import pickle
-    with open(file, 'rb') as fo:
-        dict = pickle.load(fo, encoding='bytes')
-    return dict
-
-label = unpickle('C:\\Users\\kinos\\Documents\\TU_Wien\\Deeplearning\\dlvc_ss22-main\\assignments\\Code\\dlvc\\datasets\\cifar-10-batches-py\\data_batch_2')
-print(len(label[b'labels']))
